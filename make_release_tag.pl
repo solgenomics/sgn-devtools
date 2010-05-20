cd8d31e8055c08cd255a8a9d6b1271237d456aea
Robert Buels <rmb32@cornell.edu>
2010-05-20 01:07:15 -0700

    rewrote make_release_tag.pl to be git-based

--- make_release_tag.pl
+++ make_release_tag.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use English;
+
 use Carp;
+use Data::Dumper;
 use FindBin;
 use File::Spec;
 use Getopt::Std;
-
-use Data::Dumper;
-
+use List::Util qw/ min /;
 use Pod::Usage;
-
-use CXGN::Tools::Run;
+use version;
 
 # parse and validate command line args
 my %opt;
@@ -30,26 +28,30 @@ sub fsystem(@) {
     }
 }
 
-getopts('r:V:vxmMRS:',\%opt) or pod2usage(1);
+getopts('nMmr:V:vx',\%opt) or pod2usage(1);
 
-my $svn_root = $opt{S} || 'svn+ssh://svn.sgn.cornell.edu/cxgn';
-$svn_root =~ s!/$!!; #< remove any trailing slash from the svn root
 $opt{M} || $opt{m} || $opt{V}
     or pod2usage('must specify either -M, -m, or -V');
-@ARGV == 1
-    or pod2usage('must provide component name');
+@ARGV == 1 || @ARGV == 2 or pod2usage('must provide component name, and optionally remote name');
 
-my $component_name = shift @ARGV;
+my ($component_name,$remote) = @ARGV;
+$remote = 'origin' unless defined $remote;
 
-#get a list of previous releases
-my @previous_releases = previous_releases($svn_root,$component_name);
+unless( $opt{n} ) {
+    vprint("fetching tags from $remote ...\n");
+    fsystem('git','fetch','--tags',
+            ($opt{v} ? () : '-q'),
+            $remote )
+}
+
+my @previous_releases = previous_releases($component_name);
 
 #figure out the next major version number
 my $major_version =
   defined( $opt{V} ) ? $opt{V} :
   @previous_releases ? $opt{M} ? $previous_releases[0][0]+1 :
                        $opt{m} ? $previous_releases[0][0]   :
-                       usage('must specify either -M, -m, or -V')
+                       pod2usage('must specify either -M, -m, or -V')
                      : 1
   ;
 
@@ -62,68 +64,67 @@ my $minor_version = do {
   }
 };
 
-#figure out the SVN revision of stable we'll be taking as the release
-my $release_revision = $opt{r} || 'HEAD';
+my $release_ref      = $opt{r} || 'master';
+my $new_release_tag  = "$component_name-$major_version.$minor_version";
 
-#now make the new release tag in the repository
-my $new_release_name     = "$component_name-$major_version.$minor_version";
-my $new_release_url      = "$svn_root/$component_name/tags/$new_release_name";
+fsystem( 'git', 'tag',
+         -m => "$FindBin::Script: tagging $release_ref as release $new_release_tag",
+         $new_release_tag,
+         $release_ref,
+        );
 
-if($opt{R} || $opt{m}) {
-    @previous_releases
-        or die "no previous releases, cannot do -m or -R\n";
-    my $current_release_name = "$component_name-$previous_releases[0][0].$previous_releases[0][1]";
-    my $current_release_url  = "$svn_root/$component_name/tags/$current_release_name";
-    fsystem( 'svn', 'cp',
-             -r => $release_revision,
-             -m => "make_release_tag.pl making new $component_name release $major_version.$minor_version as a copy of current release, revision '$release_revision'",
-             $current_release_url,
-             $new_release_url,
-           );
-} else {
-  fsystem( 'svn', 'cp',
-	   -r => $release_revision,
-	   -m => "make_release_tag.pl making new $component_name release $major_version.$minor_version from trunk, revision '$release_revision'",
-	   "$svn_root/$component_name/trunk",
-	   $new_release_url,
-	 );
+print <<"";
+tagged $release_ref as release $new_release_tag
+
+
+if( $opt{p} ) {
+    vprint("pushing tags to $remote ...\n");
+    fsystem(qw( git push --tags ),
+            ($opt{v} ? () : '-q'),
+            $remote );
 }
 
-print <<EOF;
-Made release '$new_release_name'.
-EOF
+
+exit;
 
 #### SUBROUTINES
 
-#args: none
-#returns: a list as ([major,minor],[major,minor]) of
-#previous revisions that are present in the repos,
-#in descending order by major and minor revision number
+# args: git remote base, component name (e.g. 'cxgn-corelibs')
+# returns: a list as ([major,minor],[major,minor]) of
+# previous revisions that are present in the repos,
+# in descending order by major and minor revision number
 sub previous_releases {
-    my ( $svn_root, $component_name ) = @_;
+    my ($component_name) = @_;
 
-    #do an svn ls and parse the output
-    my @releases;
-    my $tags_path = $svn_root."/$component_name/tags";
-    my @tags = `svn ls $tags_path`;
-    if ( @tags ) {
-	foreach my $line (@tags) {
-	    if (my ($major,$minor) = $line =~ /^$component_name-(\d+)\.(\d+)\/$/) {
-		push @releases,[$major,$minor];
-	    }
-	}
-	return sort {$b->[0] <=> $a->[0] || $b->[1] <=> $a->[1]} @releases;
-    }
-    else {
-	system
-	    'svn', 'mkdir',
-	    -m => "make_release_tag.pl initializing tags for $component_name",
-	    $tags_path;
-	return;
-    }
+    my @releases = 
+        sort { $b->[0] <=> $a->[0] || $b->[1] <=> $a->[1] }
+        map  {
+            if(m|^$component_name-(\d+)\.(\d+)(?=\s)|) {
+                [$1,$2,$&]
+            } else {
+                ()
+            }
+        } `git tag`;
+
+    vprint("last few $component_name releases:\n");
+    vprint("  $_->[2]\n") for reverse grep $_, @releases[0 .. min(2,$#releases)];
+    return @releases;
 }
 
+sub revparse {
+    my $r = shift;
+    $r = `git rev-parse $r`;
+    chomp $r;
+    return $r;
+}
 
+sub version_number {
+    my $class = shift;
+    my $tag = shift;
+    $tag =~ /(v?[\d\.]+)$/
+        or return;
+    return version->new($1);
+}
 
 __END__
 
@@ -135,9 +136,18 @@ Must specify one of -M, -m, or -V.
 
 =head1 SYNOPSIS
 
-  make_release_tag.pl [options] component_name
+  make_release_tag.pl [options] component_name [ remote_name ]
 
   Options:
+
+  -n
+     do not run 'git fetch --tags' before making a new release tag.
+     good for making a tag without network access, but be sure someone
+     has not already done it!
+
+  -p
+     run a git push --tags <remote> after creating the tag to push the
+     tag to the given remote.  be careful with this!
 
   -M
      make this a major release.  equivalent to '-V <num+1>', where
@@ -146,24 +156,15 @@ Must specify one of -M, -m, or -V.
   -m
      make this a minor release.  equivalent to '-V <num>', where
      num is the current major release.
-     Implies -R, meaning it makes the minor release as a copy of
-     the most recent release tag.
 
-  -r <num>
-     main branch revision to take as this release,
-     defaults to HEAD
-
-  -R make this release tag as a copy of the current (highest-numbered)
-     release, not a copy of the main branch
+  -r <sha>
+     the rev (branch, commit, or other tag) to take as this release,
+     defaults to 'master'
 
   -V <num>
-     major version number of this release, like '4'
+     major version number for this release, like '4'
      defaults to the next major number in the sequence
      of releases
-
-  -S <svn path>
-     path to the root of the SVN repository.  Defaults to
-     svn+ssh://svn.sgn.cornell.edu/cxgn
 
   -v be verbose about what you're doing
 
@@ -179,7 +180,7 @@ Robert Buels, E<lt>rmb32@cornell.eduE<gt>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Boyce Thompson Institute for Plant Research
+Copyright 2010 Boyce Thompson Institute for Plant Research
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
