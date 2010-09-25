678c7eeb19abe1c7d2edcbc67c6f94ce0cdd80fa
Robert Buels <rmb32@cornell.edu>
2010-09-24 18:36:26 -0700

    make pg_connection_count show connections per-database

--- pg_connection_count.sh
+++ pg_connection_count.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while [ 1 ]; do
-    sudo -u postgres psql -c "select count(*) as connections from pg_stat_activity where usename <> 'postgres'"
+    sudo -u postgres psql -c "select datname,count(*) as connections from pg_stat_activity where usename <> 'postgres' group by datname"
     sleep 1;
     clear;
 done
