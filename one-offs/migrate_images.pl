52acdfd411a35e23036212d2d94ba889a13a4811
mueller <lam87@cornell.edu>
2010-10-22 13:31:10 -0400

    added pod and options. wish had used MooseX::Runnable.

--- one-offs/migrate_images.pl
+++ one-offs/migrate_images.pl
@@ -1,13 +1,60 @@
 
+=head1 NAME
+
+migrate_images.pl - image migration script 
+
+=head1 DESCRIPTION
+
+migrates the images from a id-based directory structure to a 2 byte stemmed directory structure based on md5sums.
+
+Requires the ImageAddMD5sum.pm db patch (available in cxgn/sgn/db).
+
+Options:
+
+=over 5
+
+=item -D 
+
+database name (default sandbox)
+
+=item -H
+
+host name (default localhost)
+
+=item -o 
+
+old image location directory (default /data/prod/public/images/image_files)
+
+=item -n
+
+new image location directory (default /data/prod/public/images/new_image_files)
+
+=back
+
+=head1 AUTHOR
+
+Lukas Mueller
+
+=cut
+
 use strict;
-use CXGN::DB::Connection;
+
+use Getopt::Std;
+
+use CXGN::DB::InsertDBH;
 use CXGN::Image;
 
-my $dbh = CXGN::DB::Connection->new();
+our($opt_o, $opt_n, $opt_H, $opt_D);
 
-my $old_image_dir = '/data/prod_local/public/images/image_files';
+getopts('o:n:H:D:');
 
+my $dbh = CXGN::DB::InsertDBH->new( {
+    dbname => $opt_D || 'sandbox',
+    dbhost => $opt_H || 'localhost',
+				    });
 
+my $old_image_dir = $opt_o || '/data/prod/public/images/image_files';
+my $new_image_dir = $opt_n || '/data/prod/public/images/new_image_files';
 
 my $sql = "SELECT image_id FROM metadata.md_image where obsolete !='t' order by image_id";
 my $sth = $dbh->prepare($sql);
@@ -16,11 +63,12 @@ $sth->execute();
 my %md5sums;
 my $processed=0;
 my @not_found = ();
+
 while (my ($image_id) = $sth->fetchrow_array()) { 
 
     my $old_image = CXGN::Image->new(dbh=>$dbh, image_id=>$image_id, image_dir=> $old_image_dir);
 
-    my $i = CXGN::Image->new(dbh=>$dbh, image_id=>$image_id, image_dir=>'/data/prod_local/public/images/new_image_files');
+    my $i = CXGN::Image->new(dbh=>$dbh, image_id=>$image_id, image_dir=>$new_image_dir);
 
 
     if (! -d $old_image_dir."/".$image_id) { 
